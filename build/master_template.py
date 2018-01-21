fe6ad0905368e4b37bc5408a4a38f3d4fc84e941
kkoh <k.koh@f5.com>
2018-01-21 02:04:55 -0800

    mod master template for clibs v3.6.0

--- build/master_template.py
+++ build/master_template.py
@@ -136,7 +136,7 @@ def main():
     ### Big-IP mapped
     BIGIP_VERSION = "13.0.0.3.0.1679"
     ### Cloudlib Branch
-    branch_cloud = "release-3.6.0"
+    branch_cloud = "v3.6.0"
     branch_aws = "v1.6.0"
     branch_cloud_iapps = "v1.2.0"
     ### Build verifyHash file from published verifyHash on gitswarm. Or github (public) if gitswarm (private) not available
